6f014caa2d031f21392a91ee485e2983d021c967
Arturo Espinoza <clonjago@gmail.com>
2013-09-13 11:08:05 -0300

    cinema_cover now has default image. Cinemas dont have dependent destroy on theaters relation

--- app/models/cinema.rb
+++ app/models/cinema.rb
@@ -1,7 +1,7 @@
 class Cinema < ActiveRecord::Base
   attr_accessible :image, :information, :name, :remote_image_url, :theater_ids
   
-  has_many :theaters, :dependent => :destroy
+  has_many :theaters
   has_many :parse_detector_types
   has_and_belongs_to_many :function_types
 
--- app/uploaders/cinema_cover.rb
+++ app/uploaders/cinema_cover.rb
@@ -1,7 +1,11 @@
 # encoding: utf-8
 
 class CinemaCover < CoverUploader
-	
+  
+  def default_url
+    "/assets/personCover.png"
+  end
+  
   process :resize_to_fit => [160,160]
   process :optimize
   version :small do
