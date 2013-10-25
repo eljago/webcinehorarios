da7cea8f89d257c8bd3aea1251f0e3ef8423c114
Arturo Espinoza <clonjago@gmail.com>
2013-10-25 06:12:30 -0300

    fixed api v1 cinema show for pavilion theaters

--- app/controllers/api/v1/cinemas_controller.rb
+++ app/controllers/api/v1/cinemas_controller.rb
@@ -3,8 +3,9 @@ module Api
     class CinemasController < Api::V1::ApiController
       def show
         if params[:id] == 5
-          @cinema = Cinema.includes(:theaters).where('(theaters.cinema_id = ? OR theaters.cinema_id = 6) AND theaters.active = ?',params[:id],true)
-          .order('theaters.name ASC').all
+          @cinema = Cinema.includes(:theaters).where('theaters.cinema_id = ? AND theaters.active = ?',params[:id],true)
+          .order('theaters.name ASC').find(5)
+          @cinema.theaters << Theater.where('theaters.cinema_id = ? AND theaters.active = ?',6,true).order('theaters.name ASC')
         else
           @cinema = Cinema.includes(:theaters).where('theaters.cinema_id = ? AND theaters.active = ?',params[:id],true)
           .order('theaters.name ASC').all
