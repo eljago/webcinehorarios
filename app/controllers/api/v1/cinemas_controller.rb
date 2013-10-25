0644b5860c9883d3917877e2ccff54afd8569a1a
Arturo Espinoza <clonjago@gmail.com>
2013-10-25 06:18:36 -0300

    big fucked up fixed

--- app/controllers/api/v1/cinemas_controller.rb
+++ app/controllers/api/v1/cinemas_controller.rb
@@ -2,14 +2,8 @@ module Api
   module V1
     class CinemasController < Api::V1::ApiController
       def show
-        if params[:id] == 5
-          @cinema = Cinema.includes(:theaters).where('theaters.cinema_id = ? AND theaters.active = ?',params[:id],true)
-          .order('theaters.name ASC').find(5)
-          @cinema.theaters << Theater.where('theaters.cinema_id = ? AND theaters.active = ?',6,true).order('theaters.name ASC')
-        else
           @cinema = Cinema.includes(:theaters).where('theaters.cinema_id = ? AND theaters.active = ?',params[:id],true)
           .order('theaters.name ASC').all
-        end
       end
     end
   end
