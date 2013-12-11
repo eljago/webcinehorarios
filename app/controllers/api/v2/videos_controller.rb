cd7ec5a088a2d943c2bfa305a517de313a628399
Arturo Espinoza <clonjago@gmail.com>
2013-12-11 18:02:18 -0300

    Added Video's videoable (show) to the api v2 index action

--- app/controllers/api/v2/videos_controller.rb
+++ app/controllers/api/v2/videos_controller.rb
@@ -5,7 +5,7 @@ module Api
       def index
         page = params[:page].blank? ? 1 : params[:page]
         
-        @videos = Video.order('created_at DESC').select([:id, :name, :code, :image]).paginate(page: page, per_page: 10).all
+        @videos = Video.includes(:videoable).order('created_at DESC').select([:id, :name, :code, :image, :videoable_type, :videoable_id]).paginate(page: page, per_page: 10).all
       end
       
     end
--- app/views/api/v2/videos/index.json.rabl
+++ app/views/api/v2/videos/index.json.rabl
@@ -1,3 +1,6 @@
 collection @videos
 cache ['v2', @videos], expires_in: 1.hour
-attributes :id, :name, :code, :image_url
+attributes :id, :name, :code, :image_url, :videoable_id, :videoable_type
+child :videoable do
+	attributes :id, :name, :image_url
+end
