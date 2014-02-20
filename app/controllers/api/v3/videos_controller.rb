6ecfe6bc746f4473fa195c84303ad05c27bc33e7
Arturo Espinoza <clonjago@gmail.com>
2014-02-20 15:46:11 -0300

    api v3 videos index 15 videos per page

--- app/controllers/api/v3/videos_controller.rb
+++ app/controllers/api/v3/videos_controller.rb
@@ -6,7 +6,7 @@ module Api
         page = params[:page].blank? ? 1 : params[:page]
         
         @videos = Video.includes(:videoable).order('created_at DESC')
-        .select([:id, :name, :code, :image, :videoable_type, :videoable_id]).paginate(page: page, per_page: 10).all
+        .select([:id, :name, :code, :image, :videoable_type, :videoable_id]).paginate(page: page, per_page: 15).all
         
       end
       
