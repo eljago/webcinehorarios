de96e031fda8fd2ab38a7c2b5967be11c8c8cc67
Arturo Espinoza <clonjago@gmail.com>
2014-03-17 05:45:01 -0300

    http instead of https for carrierwave images.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,5 +14,5 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
   }
   config.fog_directory = ENV["FOG_DIRECTORY"]
-  
+  config.asset_host = "http://#{config.fog_directory}.s3.amazonaws.com"
 end
