9b668694f28535662bc87ca492decbe2af5e90da
Arturo Espinoza <clonjago@gmail.com>
2014-08-18 00:00:34 -0400

    migration that renamed an index was causing problems. Apparently the index is renamed when the movies table was renamed to shows, so this migrations became unnecesary.

--- db/migrate/20130325170312_rename_index_in_functions.rb
+++ db/migrate/20130325170312_rename_index_in_functions.rb
@@ -1,11 +1,13 @@
 class RenameIndexInFunctions < ActiveRecord::Migration
   def up
-    remove_index 'functions', name: 'index_functions_on_movie_id'
-    add_index "functions", ["show_id"], :name => "index_functions_on_show_id"
+    # remove_index :functions, :movie_id
+    # add_index :functions, :show_id
+    # # remove_index 'functions', name: 'index_functions_on_movie_id'
+    # # add_index "functions", ["show_id"], :name => "index_functions_on_show_id"
   end
 
   def down
-    remove_index 'functions', name: 'index_functions_on_show_id'
-    add_index "functions", ["movie_id"], :name => "index_functions_on_movie_id"
+    # remove_index 'functions', name: 'index_functions_on_show_id'
+    # add_index "functions", ["movie_id"], :name => "index_functions_on_movie_id"
   end
 end
