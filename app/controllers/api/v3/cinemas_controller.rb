66009dba921dbb5ff195f4dfe95d95e5ae15fa0e
Arturo <clonjago@gmail.com>
2014-12-05 04:22:09 -0300

    Cinemas#show orders theaters by name.

--- app/controllers/api/v3/cinemas_controller.rb
+++ app/controllers/api/v3/cinemas_controller.rb
@@ -3,7 +3,7 @@ module Api
     class CinemasController < Api::V3::ApiController
       
       def show
-        @cinema = Cinema.includes(:theaters).where(theaters: {active: true}).references(:theaters).find(params[:id])
+        @cinema = Cinema.includes(:theaters).where(theaters: {active: true}).references(:theaters).order('theaters.name').find(params[:id])
       end
     end
   end
