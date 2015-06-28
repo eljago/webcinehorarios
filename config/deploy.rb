7517be32ba24120e8b020c6bb4e92ae054d40ef1
Arturo <clonjago@gmail.com>
2015-06-28 08:29:47 -0300

    changed ruby version in deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :db_remote_clean, true
 set :disallow_pushing, true
 
 set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_ruby, '2.1.2'
+set :rbenv_ruby, '2.2.2'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
