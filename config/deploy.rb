6f2366b8e2d7ae4cc6296d8182e4bcc13f2338c3
Arturo <clonjago@gmail.com>
2015-10-11 05:18:04 -0300

    changed repository location at deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :rbenv_roles, :all # default value
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
 set :application, 'webcinehorarios'
-set :repo_url, 'ssh://perforce@107.170.169.97:1525/home/perforce/repos/webcinehorarios.git'
+set :repo_url, 'https://Ghar@bitbucket.org/Ghar/webcinehorarios.git'
 
 set :deploy_to, '/home/jago/apps/webcinehorarios'
 
