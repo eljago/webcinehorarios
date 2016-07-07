f641da48b54c5a0e36ccad251a80b8b02fbf51c3
Arturo Espinoza <clonjago@gmail.com>
2016-07-06 23:23:34 -0400

    using update_all again

--- app/controllers/admin/show_person_roles_controller.rb
+++ app/controllers/admin/show_person_roles_controller.rb
@@ -5,7 +5,7 @@ class Admin::ShowPersonRolesController < ApplicationController
   def sort
     if params[:show_person_roles].present?
       params[:show_person_roles].each_with_index do |id, index|
-        ShowPersonRole.find(id).update_attribute(:position, index+1)
+        ShowPersonRole.where(id: id).update_all(position: index+1)
       end
     end
     
