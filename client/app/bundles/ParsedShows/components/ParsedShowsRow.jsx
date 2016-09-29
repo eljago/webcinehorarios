83e8af090482f43990d42da3a9be517a3e596369
Arturo Espinoza <clonjago@gmail.com>
2016-09-29 04:55:24 -0300

    testing changes in production to fix parsed_shows select issue

--- client/app/bundles/ParsedShows/components/ParsedShowsRow.jsx
+++ client/app/bundles/ParsedShows/components/ParsedShowsRow.jsx
@@ -49,10 +49,10 @@ export default class ParsedShowsRow extends React.Component {
           <FormFieldSelect
             submitKey='show_id'
             label='Show'
-            ref={`show_id$`}
+            ref={'show_id'}
             initialValue={{
-              value: parsedShow.show_id,
-              label: parsedShow.show_name
+              value: parsedShow.show_id ? parsedShow.show_id : '',
+              label: parsedShow.show_name ? parsedShow.show_name : ''
             }}
             getOptions={this.props.getShowsOptions}
             onChange={(newValue) => {this._onChangeSelect(newValue)}}
