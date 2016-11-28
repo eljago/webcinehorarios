3ddba8f8b33de046af9bca917b211cf9be821fc7
Arturo Espinoza <clonjago@gmail.com>
2016-11-27 22:43:20 -0300

    added genres, show_id, rating to ShowType graphql.

--- app/graph/types/show_type.rb
+++ app/graph/types/show_type.rb
@@ -5,6 +5,11 @@ ShowType = GraphQL::ObjectType.define do
   # `id` exposes the UUID
   global_id_field :id
 
+  field :show_id, types.Int do
+    resolve ->(obj, args, ctx) {
+      obj.id
+    }
+  end
   field :name, types.String
   field :name_original, types.String
   field :active, types.Boolean
@@ -18,6 +23,12 @@ ShowType = GraphQL::ObjectType.define do
   field :metacritic_score, types.Int
   field :rotten_tomatoes_url, types.String
   field :rotten_tomatoes_score, types.Int
+  field :rating, types.String
+  field :genres, types.String do
+    resolve ->(obj, args, ctx) {
+      obj.genres.map(&:name).join(', ')
+    }
+  end
   field :cover, types.String do
     resolve ->(obj, args, ctx) {
       obj.image_url
