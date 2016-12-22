6c373c04299919c5eb4150cb230cb61c12c04700
Arturo Espinoza <clonjago@gmail.com>
2016-12-22 01:13:07 -0300

    created filter and date arguments in videos graphq field. Resolvers for filter billboard and coming_soon.

--- app/graph/fields/videos_field.rb
+++ app/graph/fields/videos_field.rb
@@ -2,7 +2,9 @@ VideosField = GraphQL::Field.define do
   name('videos')
 
   argument :cacheTime, types.String
+  argument :filter, types.String, default_value: nil
   argument :page, types.Int, default_value: 1
+  argument :date, types.String
   
   type types[VideoType]
 
